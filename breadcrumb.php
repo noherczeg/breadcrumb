af47f24a09b057541249bb4088c7075addcadafc
Norbert Csaba Herczeg <noherczeg@gmail.com>
2012-08-13 14:27:36 +0200

    finalized the class, now works properly
    Signed-off-by: Norbert Csaba Herczeg <noherczeg@gmail.com>

--- breadcrumb.php
+++ breadcrumb.php
@@ -7,8 +7,8 @@
  * Licensed under the 3-clause BSD License.
  *
  * This source file is subject to the 3-clause BSD License that is
- * bundled with this package in the LICENSE file.  It is also available at
- * the following URL: http://www.opensource.org/licenses/BSD-3-Clause
+ * available at the following URL:
+ * http://www.opensource.org/licenses/BSD-3-Clause
  *
  * @package    Breadcrumb
  * @version    1.0
@@ -22,6 +22,9 @@ namespace Breadcrumb;
 use Config;
 use Lang;
 use Str;
+use URI;
+use URL;
+use HTML;
 
 class BreadcrumbException extends \Exception {}
 
@@ -64,7 +67,7 @@ class Breadcrumb
 	/**
 	 * Splits (if it needs to) the given input into an array.
 	 *
-	 * @param   int|string|array  Whatever :)
+	 * @param   string|array  	Whatever :)
 	 * @return  array
 	 */
 	private static function split_uri($input)
@@ -95,7 +98,7 @@ class Breadcrumb
 	{
 		$result_formatted = null;
 
-		if(strlen($format == 0) || is_null($format))
+		if(strlen($format) == 0 || is_null($format))
 			$format = Config::get('breadcrumb::breadcrumb.output_format');
 
 		if (!empty(static::$segments_translated))
@@ -104,13 +107,13 @@ class Breadcrumb
 			$final_array = array();
 			$max = count(static::$segments_translated) - 1;
 
-			// ignore the not needed segments
+			// ignore not needed segments
 			for ($key = 0 + $slice_to_left; $key <= $max - $slice_from_right; $key++)
 			{
 				$final_array[] = static::$segments_translated[$key];
 			}
 
-			// decide how to display output
+			// decide what to dump as output
 			switch ($format)
 			{
 				case 'json':
@@ -133,17 +136,17 @@ class Breadcrumb
 	 * Translates the input segments if it finds a match for them in the
 	 * language files, if not, it leaves them as they are.
 	 *
-	 * @param  int|string|array     Input element
+	 * @param  string|array     	Input element
 	 * @param  string 				The casing of the breadcrumb segments
 	 * @throws BreadcrumbException
 	 * @return void
 	 */
-	public static function translate($input = null, $casing = '')
+	public static function translate($input = null, $casing = null)
 	{
-		if(strlen($casing == 0))
+		if (strlen($casing) == 0 || is_null($casing))
 			$casing = Config::get('breadcrumb::breadcrumb.default_casing');
 
-		// Check if an input was given or not and process it if it's necessary
+		// Check if an input was given or not and process it if it is necessary
 		if (is_array($input))
 		{
 			static::$segments_raw = $input;
@@ -165,7 +168,7 @@ class Breadcrumb
 
 			foreach (static::$segments_raw AS $value)
 			{
-				$key = 'breadcrumb.' . $value;
+				$key = 'breadcrumb::breadcrumb.' . $value;
 				$tmp = '';
 
 				// If the segment is in the language file it loads it, otherwise
@@ -211,30 +214,27 @@ class Breadcrumb
 	 * just call it by itself. If you coose this, it'll generate
 	 * content for the current URI with the default settings.
 	 *
-	 * - The source should be a translated dump (either a PHP array
+	 * - The source: should be a translated dump (either a PHP array
 	 * or JSON array) or left null.
 	 *
-	 * - Extra atrribute can be an array which normaly you'd pass to
+	 * - Extra atrribute: can be an array which normaly you'd pass to
 	 * Laravel's HTML::link() method.
 	 * 
-	 * - Separator should be a single caharacter or a string.
+	 * - A separator: should be a single caharacter or a string.
 	 *
-	 * - Base URL can be left as is (null) then it is handled by
-	 * calling URL::base(), or you can specify any URL for it.
-	 *
-	 * - Last not link is a toggler which makes you able to choose
+	 * - Last not link: is a toggler which makes you able to choose
 	 * if you want the last segment be a link or just a plain string.
 	 * At default it is set to true = plain string.
 	 *
-	 * @param  null|array     	The source array. Either dumped, or null
-	 * @param  null|array 		Array of attributes for the link's tag
-	 * @param  null|string 		Separator character, or string
-	 * @param  null|string 		Base url of the links
-	 * @param  bool 			Is the last element a link or plain string
+	 * @param  array     	The source array. Either dumped, or null
+	 * @param  array 		Array of attributes for the link's tag
+	 * @param  string 		Separator character, or string
+	 * @param  string 		Base url of the links
+	 * @param  bool 		Is the last element a link or plain string
 	 * @throws BreadcrumbException
 	 * @return string
 	 */
-	public static function generate_html($source = null, $extra_attrib = null, $separator = null, $base_url = null, $last_not_link = true)
+	public static function generate_html($source = null, $extra_attrib = null, $separator = null, $last_not_link = true)
 	{
 		/**
 		 * Setting up working variables, etc for the job
@@ -242,16 +242,12 @@ class Breadcrumb
 		$pretty_result = '';
 		$tmp_uri = '';
 		$working_array = array();
-		$json_test = json_decode($source);
 
 		/**
 		 * Handling nulled parameters
 		 */
 		if(is_null($separator))
 			$separator = Config::get('breadcrumb::breadcrumb.separator');
-
-		if(is_null($base_url))
-			$base_url = URL::base();
 
 		if(!is_array($extra_attrib))
 			$extra_attrib = null;
@@ -264,13 +260,13 @@ class Breadcrumb
 		{
 			$working_array = $source;
 		}
-		elseif($json_test != null)
+		elseif(json_decode($source) != null)
 		{
-			$working_array = $json_test;
+			$working_array = array_values(json_decode($source));
 		}
 		elseif(is_null($source))
 		{
-			$working_array = static::segments_translated;
+			$working_array = static::$segments_translated;
 		}
 		else
 		{
@@ -284,16 +280,26 @@ class Breadcrumb
 		 * the parameters the last element is a simple string, or a
 		 * link it self too.
 		 */
+		end($working_array);
+		$last_key = key($working_array);
+
 		foreach($working_array AS $key => $segment)
 		{
-			$tmp_uri .= $segment . '/';
-
-			if($key == end($working_array) && $last_not_link == true)
+			// only place separator after first segment :)
+			if($key > 0)
 			{
-				$pretty_result .= ' ' . $separator . ' ' . $segment;
+				$pretty_result .= ' ' . trim($separator) . ' ';
 			}
 
-			$pretty_result .= $pretty_result .= ' ' . $separator . ' ' . HTML::link($tmp_uri, $segment, $extra_attrib);
+			if($key == $last_key && $last_not_link == true)
+			{
+				$pretty_result .= $segment;
+			}
+			else
+			{
+				$tmp_uri .= $segment . '/';
+				$pretty_result .= HTML::link($tmp_uri, $segment, $extra_attrib);
+			}
 		}
 
 		return $pretty_result;
