0e3af7d3c98d0d0a5c7e98dd23d8ff5e4047c4e3
Norbert Csaba Herczeg <noherczeg@gmail.com>
2012-08-13 14:26:57 +0200

    edited comments
    Signed-off-by: Norbert Csaba Herczeg <noherczeg@gmail.com>

--- config/breadcrumb.php
+++ config/breadcrumb.php
@@ -7,8 +7,8 @@
  * Licensed under the 3-clause BSD License.
  *
  * This source file is subject to the 3-clause BSD License that is
- * bundled with this package in the LICENSE file.  It is also available at
- * the following URL: http://www.opensource.org/licenses/BSD-3-Clause
+ * available at the following URL:
+ * http://www.opensource.org/licenses/BSD-3-Clause
  *
  * @package    Breadcrumb
  * @version    1.0
@@ -29,7 +29,7 @@ return array(
 	/**
 	 * Default output format.
 	 *
-	 * You can choose from: array, josn.
+	 * You can choose from: array, json.
 	 */
 	'output_format' => 'array',
 
