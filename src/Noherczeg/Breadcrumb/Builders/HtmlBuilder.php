a22acbbd204f4cc920f8690e908280714c04964f
Norbert Csaba Herczeg <noherczeg@gmail.com>
2012-12-16 21:34:33 +0100

    reordered parameters, fixed property injection

--- src/Noherczeg/Breadcrumb/Builders/HtmlBuilder.php
+++ src/Noherczeg/Breadcrumb/Builders/HtmlBuilder.php
@@ -11,13 +11,13 @@ class HtmlBuilder extends Builder
     /**
      * build: The builder method which creates HTML style breadcrumbs
      * 
-     * @param String|null $separator    Separator String
      * @param String|null $casing       Casing option
      * @param boolean $last_not_link    True if last shouldn't be a link
+     * @param String|null $separator    Separator String
      * @param array $customizations     Array of properties + values
      * @return String
      */
-    public function build ($separator = null, $casing = null, $last_not_link = true, $properties = array())
+    public function build ($casing = null, $last_not_link = true, $separator = null, $properties = array())
     {
         // always create link on build stage!
         $this->link($last_not_link);
@@ -37,7 +37,7 @@ class HtmlBuilder extends Builder
             }
             
 			if (is_null($segment->get('link'))) {
-				$result .= $this->casing($segment->get('translated'), $tc);
+				$result .= '<span' . $this->properties($properties) . '>' . $this->casing($segment->get('translated'), $tc) . '</span>';
 			} else {
 				$result .= '<a href="' . $segment->get('link') . '" ' . $this->properties($properties) . '>' . $this->casing($segment->get('translated'), $tc) . '</a>';
 			}
