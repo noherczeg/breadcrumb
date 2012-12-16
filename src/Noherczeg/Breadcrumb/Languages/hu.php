c72f9cd515dbe2ddf9b6d0bfc30e0d3ccbbeb49c
Norbert Csaba Herczeg <noherczeg@gmail.com>
2012-12-16 21:36:37 +0100

    added: new example language file

---
+++ src/Noherczeg/Breadcrumb/Languages/hu.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Language: HU
+ *
+ * Hungarian language for breadcrumb translations.
+ */
+
+return array (
+    /**
+     * Always use the same separator in the translated keys which is set in the
+     * config file => slug_separator setting!
+     */
+    'test-segment'  => 'ez egy próba',
+    'second' => '1234 stb..'
+);
